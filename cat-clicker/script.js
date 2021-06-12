32f170d23ef17bf7df8b5b45f3d0a3cc7349ffc4
Sutthinart Khunvadhana <sutthinart.khunvadhana@refinitiv.com>
2021-06-13 05:43:38 +0700

    requirement 3 cat clicker

--- cat-clicker/script.js
+++ cat-clicker/script.js
@@ -1,19 +1,56 @@
-function start() {
-    let totalCat1Click = 0;
-    const cat1Elem = document.getElementById("cat1-image");
-    const cat1ClickDisplay = document.getElementById("cat1-click");
-    cat1Elem.addEventListener("click", () => {
-        totalCat1Click++;
-        cat1ClickDisplay.innerHTML = totalCat1Click;
-    });
+const catList = [
+    {
+        name: "Meow-1",
+        photo: "cat1.jpg",
+        click: 0
+    },
+    {
+        name: "Meow-2",
+        photo: "cat2.jpg",
+        click: 0
+    },
+    {
+        name: "Meow-3",
+        photo: "cat3.jpg",
+        click: 0
+    },
+    {
+        name: "Meow-4",
+        photo: "cat4.jpg",
+        click: 0
+    },
+    {
+        name: "Meow-5",
+        photo: "cat5.jpg",
+        click: 0
+    }
+];
 
-    let totalCat2Click = 0;
-    const cat2Elem = document.getElementById("cat2-image");
-    const cat2ClickDisplay = document.getElementById("cat2-click");
-    cat2Elem.addEventListener("click", () => {
-        totalCat2Click++;
-        cat2ClickDisplay.innerHTML = totalCat2Click;
-    });
+function start() {
+    for (let cat of catList) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("cat-title");
+        const heading = document.createElement("h3");
+        heading.innerHTML = cat.name;
+        heading.addEventListener("click", () => {
+            const catName = document.getElementById("name");
+            const click = document.getElementById("click");
+            const bg = document.getElementById("cat-display");
+
+            catName.innerHTML = cat.name;
+            click.innerHTML = cat.click;
+            bg.style.backgroundImage = `url('${cat.photo}')`;
+            const clickHandler = (c) => {
+                cat.click = cat.click + 1;
+                click.innerHTML = cat.click;
+            };
+            bg.removeEventListener('click', clickHandler);
+            bg.addEventListener('click', clickHandler);
+        });
+        newDiv.appendChild(heading);
+        const catListElem = document.querySelector(".cat-list");
+        catListElem.appendChild(newDiv);
+    }
 }
 
 start();
