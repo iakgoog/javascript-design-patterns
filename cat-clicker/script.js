53777e15500bf142589fec7fa1a8097a03758175
Sutthinart Khunvadhana <sutthinart.khunvadhana@refinitiv.com>
2021-06-12 07:25:52 +0700

    requirement 2 cat clicker

--- cat-clicker/script.js
+++ cat-clicker/script.js
@@ -1,9 +1,18 @@
 function start() {
-    let totalClick = 0;
-    const catWrapperElem = document.querySelector(".cat-wrapper");
-    catWrapperElem.addEventListener("click", () => {
-        totalClick++;
-        console.log(totalClick);
+    let totalCat1Click = 0;
+    const cat1Elem = document.getElementById("cat1-image");
+    const cat1ClickDisplay = document.getElementById("cat1-click");
+    cat1Elem.addEventListener("click", () => {
+        totalCat1Click++;
+        cat1ClickDisplay.innerHTML = totalCat1Click;
+    });
+
+    let totalCat2Click = 0;
+    const cat2Elem = document.getElementById("cat2-image");
+    const cat2ClickDisplay = document.getElementById("cat2-click");
+    cat2Elem.addEventListener("click", () => {
+        totalCat2Click++;
+        cat2ClickDisplay.innerHTML = totalCat2Click;
     });
 }
 
